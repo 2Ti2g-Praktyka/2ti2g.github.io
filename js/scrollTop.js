878119dcdbe6adef81033d1ab28c31d0080a5857
2Ti2g-Praktyka <78430421+2Ti2g-Praktyka@users.noreply.github.com>
2021-02-11 18:32:01 +0100

    Added new files

--- js/scrollTop.js
+++ js/scrollTop.js
@@ -3,13 +3,13 @@ var j = jQuery.noConflict();
 j(document).ready(function(){
 	j(window).scroll(function () {
 			if (j(this).scrollTop() > 40) {
-				j("#back-to-top").fadeIn();
+				j("#go-top").fadeIn();
 			} else {
-				j("#back-to-top").fadeOut();
+				j("#go-top").fadeOut();
 			}
 		});
 		// scroll body to 0px on click
-		j("#back-to-top").click(function () {
+		j("#go-top").click(function () {
 			j("body,html").animate({
 				scrollTop: 0
 			}, 350, "linear");
